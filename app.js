c21f40cc05ca44b807a9b60b4db93f4be997f296
MÔNICA RIBEIRO DA SILVA <22282167@CFP127ALUNO.LOCAL>
2023-03-20 11:30:22 -0300

    testando

--- app.js
+++ app.js
@@ -2,13 +2,24 @@
 
 const mapa = document.querySelector('#map')
 
+const sigla = document.getElementById('sigla')
+
 const getEstado = ( {target} ) => {
     
     const estado = target.id.replace('BR-','')
     // const nomeEstado = target.getAttribute('title')
     // console.log(nomeEstado)
 
-    // preencherDados(estado)
+    sigla.innerHTML = estado;
+}
+
+// preencherDados(estado);
+const preencherDados = async (estado) => {
+    
+    const sigla = getEstado(estado)
 }
 
 mapa.addEventListener('click', getEstado)
+
+
+// http://localhost:8080/v1/senai/cidades?uf=SP
